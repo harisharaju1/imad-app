29ffb805a5b1fb9f54f616f426fd7c79bf476804
Harish Raju <harisharaju1@gmail.com>
2017-09-18 19:58:24 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,3 +2,10 @@ console.log('Loaded!');
 
 var element = document.getElementById('main-text');
 element.innerHTML = 'New value';
+
+//move the image
+
+document.getElementById('img');
+img.onclick = function() {
+    img.style.marginLeft = '100px';
+};
