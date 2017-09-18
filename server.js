66bf723cabae703d645cc052aee99cebfd227cac
Harish Raju <harisharaju1@gmail.com>
2017-09-18 19:24:38 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,20 +5,102 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var articleOne = {
+    title: 'Article One | Harish Raju',
+    heading: 'Article One',
+    data: 'Sep 18, 2017',
+    content:`
+        <p>
+            This is the content that is going to be displayed when I call it . YO!
+        </p>
+        <p>
+            This is the content that is going to be displayed when I call it . YO!
+        </p>
+        <p>
+            This is the content that is going to be displayed when I call it . YO!
+        </p>`
+};
+
+var articleTwo = {
+    title: 'Article Two | Harish Raju',
+    heading: 'Article Two',
+    date: 'Sep 18, 2017',
+    content:`
+        <p>
+            This is the content that is going to be displayed when I call it . YO!
+        </p>
+        <p>
+            This is the content that is going to be displayed when I call it . YO!
+        </p>
+        <p>
+            This is the content that is going to be displayed when I call it . YO!
+        </p>`
+}
+
+var articleThree = {
+    title: 'Article Three | Harish Raju',
+    heading: 'Artcile Three',
+    date: 'Sep 18, 2017',
+    content:`
+        <p>
+            This is the content that is going to be displayed when I call it . YO!
+        </p>
+        <p>
+            This is the content that is going to be displayed when I call it . YO!
+        </p>
+        <p>
+            This is the content that is going to be displayed when I call it . YO!
+        </p>`
+}
+
+function createTEmplate (data) { 
+    var title = data.title;
+    var date = data.date;
+    var heading = data.heading;
+    var content = data.content;
+    var htmlTemplate = `<html>
+        <head>
+            <title>
+                ${title}
+            </title>
+            <meta name="viewport" content="width=device-width, initial-scale=1"/>
+            <link href="/ui/style.css" rel="stylesheet" />
+        </head>
+        <body>
+            <div class="container">
+                <div>
+                    <a href = "/"> Home </a>
+                </div>
+                <hr/>
+                <h3>
+                    ${heading}
+                </h3>
+                <div>
+                    ${date}
+                </div>
+                <div>
+                    ${content}
+                </div>
+            </div>
+        </body>
+    </html> `;
+    return htmlTemplate;
+}
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/article-one', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+  res.send(createTemplate(articleOne));
 });
 
 app.get('/article-two', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
+  res.send(createTemplate(articleTwo));
 });
 
 app.get('/article-three', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
+  res.send(createTemplate(articleThree));
 });
 
 app.get('/ui/style.css', function (req, res) {
