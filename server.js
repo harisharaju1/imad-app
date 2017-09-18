b1b860d2d26d4b064aff43bc858b792a9c3ab95b
Harish Raju <harisharaju1@gmail.com>
2017-09-18 20:56:06 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -92,6 +92,14 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+var names = [];
+app.get('/submit-name/:name', function(req,res) {
+    //Get the name from the request
+    var name = req.params.name;
+    names.push(name);
+    //JSON = JavaScript Object Notation
+    res.send(JSON.stringify(names));
+})
 var counter = 0;
 app.get('/counter', function(req,res) {
     counter = counter + 1;
