5df033fbc577128a39ed355bb28579356e504bab
Harish Raju <harisharaju1@gmail.com>
2017-09-18 20:32:41 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,8 +1,21 @@
 var button = document.getElementById('counter');
 var counter = 0;
 button.onclick = function() {
-    //Render ther variable in the span
-    counter = counter + 1;
-    var span = document.getElementById('count');
-    span.innerHTML = counter.toString();
-}
+    //Make a request to the counter end-point
+    var request = new XMLHttpRequest();
+    
+    
+    //Capture the response and store it in a variable
+    request.onreadystatechange = function() {
+        if(request.readyState === XMLHttpRequest.DONE);
+        //Take some action
+        if(request.status == 200) {
+            var counmter = request.responseTExt;
+            var span = documenr.getElementById('count');
+            span.innerHTML = Counter.toString();
+        }
+    };
+
+    request.open('GET' ,'http://harisharaju1.imad.hasura-app.io', true);
+    request.send(null);
+};
