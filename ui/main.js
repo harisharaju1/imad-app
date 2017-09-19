cbaf0c23b3d5032005206489d5b003c54e3c26e7
Harish Raju <harisharaju1@gmail.com>
2017-09-19 12:04:59 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -22,8 +22,7 @@ button.onclick = function() {
 };
 
 //Submit name
-var nameInput = document.getElementById('name');
-var name = nameInput.value;
+
 var submit = document.getElementById('submit_btn');
 submit.onclick = function() {
     
@@ -47,7 +46,8 @@ submit.onclick = function() {
             }
         }
     };
-
+    var nameInput = document.getElementById('name');
+    var name = nameInput.value;
     request.open('GET', 'http://harisharaju1.imad.hasura-app.io/submit-name?name=' + name, true);
     request.send(null);
 };
