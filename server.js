a291f7dd1c67be1d9d14c52d672cf145bbb876bf
Harish Raju <harisharaju1@gmail.com>
2017-09-29 16:48:22 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -114,6 +114,9 @@ app.get('/:articleName', function (req, res) {
   res.send(createTemplate(articles[articleName]));
 });
 
+app.get('/comment', function(req, res) {
+   res.send('Ready to receive comment content.'); 
+});
 app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
